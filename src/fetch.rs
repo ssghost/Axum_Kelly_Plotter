ae39b18f5aed80c4f1f1adb12aaeb1629644e98d
ssghost <ssprof0@gmail.com>
2023-04-23 00:56:40 +0800

    ..

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,46 +1,18 @@
-use exitfailure::ExitFailure;
-use reqwest::Url;
-use serde_derive::{Deserialize, Serialize};
 use std::env;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct CompanyQuote {
-    c: f64,
-    h: f64,
-    l: f64,
-    o: f64,
-    pc: f64,
-    t: i128,
-}
-
-impl CompanyQuote {
-    async fn get(symbol: &String, api_key: &String) -> Result<Self, ExitFailure> {
-        let url = format!(
-            "https://finnhub.io/api/v1/quote?symbol={}&token={}",
-            symbol, api_key
-        );
-
-        let url = Url::parse(&*url)?;
-        let res = reqwest::get(url).await?.json::<CompanyQuote>().await?;
-
-        Ok(res)
-    }
-}
+use yahoo_finance::{history, Interval, Timestamped};
 
 #[tokio::main]
-async fn main() -> Result<(), ExitFailure> {
-    let api_key = "YOUR API KEY".to_string();
-    let args: Vec<String> = env::args().collect();
+pub async fn main() -> Result<(), ExitFailure>{
     let mut symbol: String = "AAPL".to_string();
-
-    if args.len() < 2 {
-        println!("Since you didn't specify a company symbol, it has defaulted to AAPL.");
+    let data = history::retrieve_interval("AAPL", Interval::_6mo).await.unwrap();
+    let mut symbol: String = "AAPL".to_string();
+    if args.len() < 1 {
+        println!("Using default symbol AAPL.");
     } else {
         symbol = args[1].clone();
     }
-
-    let res = CompanyQuote::get(&symbol, &api_key).await?;
-    println!("{}'s current stock price: {}", symbol, res.c);
-
-    Ok(())
+    for bar in &data {
+      println!("Apple hit an intraday high of ${:.2} on {}.", bar.high, bar.datetime().format("%b %e %Y"));
+    }
 }
