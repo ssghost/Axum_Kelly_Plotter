3900301dfcc0c3c5015af15fac8a63443e909776
ssghost <ssprof0@gmail.com>
2023-04-23 20:09:17 +0800

    finish fetch

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,18 +1,44 @@
 use std::env;
-
-use yahoo_finance::{history, Interval, Timestamped};
+use yew::prelude::*;
+use web_sys::HtmlInputElement;
+use yahoo_finance::{history, Interval, Timestamped, Result};
 
 #[tokio::main]
-pub async fn main() -> Result<(), ExitFailure>{
-    let mut symbol: String = "AAPL".to_string();
-    let data = history::retrieve_interval("AAPL", Interval::_6mo).await.unwrap();
-    let mut symbol: String = "AAPL".to_string();
-    if args.len() < 1 {
-        println!("Using default symbol AAPL.");
-    } else {
-        symbol = args[1].clone();
-    }
-    for bar in &data {
-      println!("Apple hit an intraday high of ${:.2} on {}.", bar.high, bar.datetime().format("%b %e %Y"));
+#[function_component]
+pub async fn Fetch() -> Html{
+    let input_node_ref = use_node_ref();
+    let symbol = use_state<String>(|| "AAPL");
+    let data = use_state<Result>();
+
+    let onchange = {
+        let input_node_ref = input_node_ref.clone();
+        Callback::from(move |_| {
+            if let Some(input) = input_node_ref.cast::<HtmlInputElement>() {
+                symbol.set(input.value());
+            } 
+        })      
+    };
+    let onclick = {
+        let data = data.clone();
+        Callback::from(move |_| {
+            data.set(history::retrieve_interval(symbol, Interval::_6mo).await.unwrap())
+        })
+    };
+
+    html! {
+        <>
+            <label for="input_symbol">
+                { "Input the symbol to be plotted:" }
+                <input ref={input_node_ref}
+                    {onchange}
+                    id="input_symbol"
+                    type="text"
+                    value={symbol}
+                />
+            </label>
+            <div>
+            <button {onclick}>{ "Fetch data" }</button>
+            </div>
+        </>
     }
 }
